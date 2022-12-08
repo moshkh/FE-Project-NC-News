2b4e1bb48c6db997356f9644c19ae206e338c3c2
Musab Hussain <moshkhussain@gmail.com>
2022-12-08 11:40:57 +0000

    saving progress

--- src/Components/Topics.jsx
+++ src/Components/Topics.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 const Topics = () => {
+  const [topics, setTopics] = useState([]);
+  const [loading, isLoading] = useState(false);
+
+
+  // api call for topics
+  // export const getTopics = () => {
+  //   return ncNewsApi.get("/topics").then((data) => {
+  //     console.log(data);
+  //   });
+  // };
+
   return (
     <main>
       <h2>Topics</h2>
